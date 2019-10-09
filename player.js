383c0467c3a54227be30ac2c76b7cd563c8cc6f8
mk210 <55575735+mk210@users.noreply.github.com>
2019-10-09 19:39:28 +0400

    New code for placing Players.
    This is a still under testing.. not yet working

--- player.js
+++ player.js
@@ -1,13 +1,25 @@
-var canvas = document.getElementById('GameScreen'),
-context = canvas.getContext('2d');
-
-make_base();
-
-function make_base()
-{
-  base_image = new Image();
-  base_image.src = 'p1.png';
-  base_image.onload = function(){
-    context.drawImage(base_image, 20, 20);
-  }
+//Get the canvas element by using the getElementById method.
+var canvas = document.getElementById('gamescreen');
+ 
+//Get a 2D drawing context for the canvas.
+var context = canvas.getContext('2d');
+ 
+//The path to the image that we want to add.
+var imgPath = 'p1.png';
+//Create a new Image object.
+var imgObj = new Image();
+ 
+//Set the src of this Image object.
+imgObj.src = imgPath;
+ 
+//the x coordinates
+var x = 0;
+ 
+//the y coordinates
+var y = 0;
+ 
+//When our image has loaded.
+imgObj.onload = function(){
+    //Draw the image onto the canvas.
+    context.drawImage(imgObj, x, y);
 }
