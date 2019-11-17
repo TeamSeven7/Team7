65974c89ddb981599cfde3974f5d2fb2aa71a859
mk210 <55575735+mk210@users.noreply.github.com>
2019-11-17 20:14:44 +0400

    updated some files for hosting

--- Client/multiplayer.js
+++ Client/multiplayer.js
@@ -1,5 +1,6 @@
 const express=require ("express");
 const app=express();
+var serveStatic = require('serve-static')
 const dataparser=require("body-parser");
 const urlencodedParser=
 dataparser.urlencoded({extended:false});
@@ -9,18 +10,16 @@ dataparser.urlencoded({extended:false});
 //     res.sendFile('C:\Users\mohit\Documents\GitHub\Team7\#Level1\Level1.html',{root : __dirname});
 // }
 // );
-app.use(express.static('Client/img'))
+app.use(express.static('img'))
 app.get ('/home',function(req,res)
 { 
     res.sendFile('C:/Users/mohit/Documents/GitHub/Team7/Client/Homepage.html');
 }
 );
 
-
-
-app.get ('/game',function(req,res)
+app.get ('/level1',function(req,res)
 { 
-    res.sendFile('C:/Users/mohit/Documents/GitHub/Team7/#Level1/Level1.html');
+    res.sendFile('C:/Users/mohit/Documents/GitHub/Team7/%23Level1/Level1.html');
 }
 );
 
