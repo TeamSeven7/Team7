fee2d87b4d85cc778cd29c9f1e455220f1ee94c4
rk-92 <55647446+rk-92@users.noreply.github.com>
2019-11-22 17:42:07 +0400

    Hosted homepage functioning

--- Client/multiplayer.js
+++ Client/multiplayer.js
@@ -10,29 +10,29 @@ var bodyParser = require('body-parser');
 //     next();
 // });
 
-app.use(express.static('C:/User/mohit/Documents/GitHub/Team7/Client/img'));
+app.use(express.static('C:/User/2001a/Documents/GitHub/Team7/Client/img'));
 app.get ('/',function(req,res)
 { 
     res.sendFile('homepage.html',{root : __dirname});
 });
 // app.post()
 
-app.use(express.static('C:/User/mohit/Documents/GitHub/Team7/Client/LEVEL1'));
+app.use(express.static('C:/User/2001a/Documents/GitHub/Team7/Client/LEVEL1'));
 app.get ('/level1',function(req,res)
 { 
-    res.sendFile('C:/Users/mohit/Documents/GitHub/Team7/Client/LEVEL1/Level1.html');
+    res.sendFile('C:/Users/2001a/Documents/GitHub/Team7/Client/LEVEL1/Level1.html');
 });
 
-app.use(express.static('C:/Users/mohit/Documents/GitHub/Team7/Client'));
+app.use(express.static('C:/Users/2001a/Documents/GitHub/Team7/Client'));
 app.get ('/Helppage',function(req,res)
 { 
-    res.sendFile('C:/Users/mohit/Documents/GitHub/Team7/Client/Helppage.html');
+    res.sendFile('C:/Users/2001a/Documents/GitHub/Team7/Client/Helppage.html');
 });
 
-app.use(express.static('C:/Users/mohit/Documents/GitHub/Team7/LEVEL2'));
+app.use(express.static('C:/Users/2001a/Documents/GitHub/Team7/LEVEL2'));
 app.get ('/resume',function(req,res)
 { 
-    res.sendFile('C:/Users/mohit/Documents/GitHub/Team7/LEVEL2/level2-updated.html');
+    res.sendFile('C:/Users/2001a/Documents/GitHub/Team7/LEVEL2/level2-updated.html');
 }
 );
 
